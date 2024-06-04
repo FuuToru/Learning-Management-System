c8bd3065ad66399abd70d32043ff2e85958496ef
Tri <huutri231103@gmail.com>
2024-06-05 01:35:17 +0700

    setup: generic views

--- src/backend_api/main/views.py
+++ src/backend_api/main/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import generics
+from .serializers import TeacherSerializer
+from . import models
 
 # Create your views here.
+class TeacherList(generics.ListCreateAPIView):
+    queryset = models.Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
+class TeacherDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Teacher.objects.all()
+    serializer_class = TeacherSerializer
