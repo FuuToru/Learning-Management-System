81b058ffda9f304fa8c30aa19b566f3054bc5205
Tri <huutri231103@gmail.com>
2024-06-04 15:13:49 +0700

    setup: create django models & register in admin

--- src/backend_api/main/models.py
+++ src/backend_api/main/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+# Teacher Model
+class Teacher(models.Model):
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+    qualification = models.CharField(max_length=100)
+    mobile_no = models.CharField(max_length=20)
+    address = models.TextField()
+    class Meta:
+        verbose_name_plural = '1. Teachers'
+
+# Course Category Model
+class CourseCategory(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+    class Meta:
+        verbose_name_plural = '2. Course Categories'
+
+# Course Model
+class Course(models.Model):
+    category = models.ForeignKey(CourseCategory, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    class Meta:
+        verbose_name_plural = '3. Courses'
+
+class Student(models.Model):
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+    qualification = models.CharField(max_length=100)
+    mobile_no = models.CharField(max_length=20)
+    address = models.TextField()
+    interested_categories = models.TextField()
+    class Meta:
+        verbose_name_plural = '4. Students'
+
+
+
+
