4ea766f46da0dc745ccf24e3190f6aa39750ede8
Tri <huutri231103@gmail.com>
2024-06-05 01:39:46 +0700

    DRF Authentication and Permissions

--- src/backend_api/main/views.py
+++ src/backend_api/main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import generics
+from rest_framework import permissions
 from .serializers import TeacherSerializer
 from . import models
 
@@ -9,7 +10,9 @@ from . import models
 class TeacherList(generics.ListCreateAPIView):
     queryset = models.Teacher.objects.all()
     serializer_class = TeacherSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 class TeacherDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Teacher.objects.all()
     serializer_class = TeacherSerializer
+    permission_classes = [permissions.IsAuthenticated]
