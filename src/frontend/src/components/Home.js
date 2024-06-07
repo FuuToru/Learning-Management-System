9f7b08528c2d92964b01716add5e4be79707a19e
Tri <huutri231103@gmail.com>
2024-06-07 19:50:59 +0700

    front: add footer

--- src/frontend/src/components/Home.js
+++ src/frontend/src/components/Home.js
@@ -3,7 +3,7 @@ import logo from "../logo.svg";
 function Home() {
   return (
     <div className="container mt-4">
-      <h3 className="pb-1 mb-4 ">Latest Courses</h3>
+      <h3 className="pb-1 mb-4 text-start">Latest Courses <a to="/categories" className='float-end btn btn-sm btn-dark m-2'>View All Categories<i className="fa-solid fa-arrow-right"></i></a></h3>
       <div className="row">
         <div className="col-md-3">
           <div className="card">
@@ -52,7 +52,7 @@ function Home() {
         </div>
 
       </div>
-      <h3 className="pb-1 mt-4 mb-4 ">Popular Courses</h3>
+      <h3 className="pb-1 mt-4 mb-4 text-start ">Popular Courses <a to="/categories" className='float-end btn btn-sm btn-dark m-2'>View All Categories<i className="fa-solid fa-arrow-right"></i></a></h3>
       <div className="row">
         <div className="col-md-3">
           <div className="card">
@@ -93,6 +93,55 @@ function Home() {
             <div className="card-body">
               <h5 className="card-title">Course Title</h5>
               <p className="card-text">Course Description</p>
+              <a href="#" className="btn btn-primary">Enroll Now</a>
+            </div>
+          </div>
+
+
+        </div>
+
+      </div>
+      <h3 className="mb-4 mt-4 text-start ">Featured Teachers <a to="/categories" className='float-end btn btn-sm btn-dark m-2'>View All Categories<i className="fa-solid fa-arrow-right"></i></a></h3>
+      <div className="row">
+        <div className="col-md-3">
+          <div className="card">
+            <a href="#"><img src={logo} className="card-img-top" alt="..." ></img></a>
+            <div className="card-body">
+              <h5 className="card-title">Teacher Name</h5>
+              <p className="card-text">Teacher Description</p>
+              <a href="#" className="btn btn-primary">Enroll Now</a>
+            </div>
+          </div>
+
+
+        </div>
+        <div className="col-md-3">
+          <div className="card">
+            <a href="#"><img src={logo} className="card-img-top" alt="..." ></img></a>
+            <div className="card-body">
+              <h5 className="card-title">Teacher Name</h5>
+              <p className="card-text">Teacher Description</p>
+              <a href="#" className="btn btn-primary">Enroll Now</a>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div className="card">
+            <a href="#"><img src={logo} className="card-img-top" alt="..." ></img></a>
+            <div className="card-body">
+              <h5 className="card-title">Teacher Name</h5>
+              <p className="card-text">Teacher Description</p>
+              <a href="#" className="btn btn-primary">Enroll Now</a>
+            </div>
+          </div>
+
+        </div>
+        <div className="col-md-3">
+          <div className="card">
+            <a href="#"><img src={logo} className="card-img-top" alt="..." ></img></a>
+            <div className="card-body">
+              <h5 className="card-title">Teacher Name</h5>
+              <p className="card-text">Teacher Description</p>
               <a href="#" className="btn btn-primary">Enroll Now</a>
             </div>
           </div>
--- src/frontend/src/components/Main.js
+++ src/frontend/src/components/Main.js
@@ -1,10 +1,13 @@
 import Home from "./Home";
 import Header from "./Header";
+import Footer from "./footer";
 function Main() {
     return (
       <div className="App">
         <Header/>
         <Home/>
+        <Footer/>
+        
       </div>
     );
   }
