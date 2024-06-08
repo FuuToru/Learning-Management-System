38c678b133a99975946156ef861e6fcf48efcb2c
Tri <huutri231103@gmail.com>
2024-06-08 17:13:29 +0700

    front: add route

--- src/frontend/src/components/Header.js
+++ src/frontend/src/components/Header.js
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom";
+
 function Header() {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container">
-          <a className="navbar-brand" href="#">Learn Online</a>
+          <Link className="navbar-brand" to="/">Learn Online</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
--- src/frontend/src/components/Main.js
+++ src/frontend/src/components/Main.js
@@ -1,11 +1,16 @@
 import Home from "./Home";
 import Header from "./Header";
 import Footer from "./footer";
+import {Routes, Route} from 'react-router-dom';
+
 function Main() {
     return (
       <div className="App">
         <Header/>
-        <Home/>
+        <Routes >
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+        
         <Footer/>
         
       </div>
